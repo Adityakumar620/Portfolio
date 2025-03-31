440b1fb69967735f4cc1a27773d66ea75199e7d4
Aditya kumar <arajsg8226@gmail.com>
2025-03-31 22:11:04 +0530

    Fixed  mobile problem

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -21,9 +21,10 @@
 //       <pointLight intensity={1} />
 //       <primitive
 //         object={computer.scene}
-//         scale={isMobile ? 0.7 : 0.75}
+//         scale={isMobile ? 0.7 : 1.25}
 //         position={isMobile ? [0, -3, -2.2] : [0, -4.25, -1.5]}
 //         rotation={[-0.01, -0.2, -0.1]}
+        
 //       />
 //     </mesh>
 //   );
